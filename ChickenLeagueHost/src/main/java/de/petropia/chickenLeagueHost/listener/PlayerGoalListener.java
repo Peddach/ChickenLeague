46942207639acf56f28226fe06b56747cdf87e73
ProfessorSam <Samderprofessor@gmail.com>
2022-07-01 17:58:16 +0200

    Add goal effects

--- ChickenLeagueHost/src/main/java/de/petropia/chickenLeagueHost/listener/PlayerGoalListener.java
+++ ChickenLeagueHost/src/main/java/de/petropia/chickenLeagueHost/listener/PlayerGoalListener.java
@@ -3,12 +3,22 @@ package de.petropia.chickenLeagueHost.listener;
 import java.time.Duration;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.Particle;
+import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+import com.destroystokyo.paper.ParticleBuilder;
 
 import de.petropia.chickenLeagueHost.Constants;
+import de.petropia.chickenLeagueHost.arena.Arena;
 import de.petropia.chickenLeagueHost.arena.GoalCountDown;
 import de.petropia.chickenLeagueHost.events.PlayerGoalEvent;
+import de.petropia.chickenLeagueHost.team.ChickenLeagueTeam;
 import de.petropia.chickenLeagueHost.util.MessageSender;
 import net.kyori.adventure.sound.Sound;
 import net.kyori.adventure.sound.Sound.Source;
@@ -23,8 +33,10 @@ public class PlayerGoalListener implements Listener {
 	private static final Component SUBTITLE = Component.text(" hat ein Tor geschossen").color(NamedTextColor.GRAY).decorate(TextDecoration.ITALIC);
 	private static final Times TIMES = Times.times(Duration.ofMillis(500), Duration.ofMillis(1500), Duration.ofMillis(300));
 	private static final Sound SOUND = Sound.sound(org.bukkit.Sound.EVENT_RAID_HORN.getKey(), Source.NEUTRAL, 200F, 1.2F);
+	private static final Sound EXPLOSION = Sound.sound(org.bukkit.Sound.ENTITY_GENERIC_EXPLODE.getKey(), Source.NEUTRAL, 2F, 1F);
 	private Component name = Component.text("-").color(NamedTextColor.GRAY);
-	
+	private int fireworkTaskID;
+	private int endFireWorkID;
 	@EventHandler
 	public void onPlayerGoal(PlayerGoalEvent event) {
 		if(event.getPlayer() != null) {
@@ -38,11 +50,70 @@ public class PlayerGoalListener implements Listener {
 		event.getArena().getBall().getChicken().setVisualFire(true);
 		if(event.getTeam().getScore() == 5) {
 			event.getArena().setWinner(event.getTeam());
+			showEffects(event.getArena(), event.getTeam());
+			endFireWorkID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Constants.plugin, new Runnable() {
+				int i = 0;
+				@Override
+				public void run() {
+					if(i == 15) {
+						Bukkit.getScheduler().cancelTask(endFireWorkID);
+					}
+					spwanFireWork(event.getArena(), event.getTeam());
+				}
+			}, 120, 20);
 			return;
 		}
 		Bukkit.getScheduler().runTaskLater(Constants.plugin, () -> {
 			new GoalCountDown(event.getArena());
 		}, 3*20);
+		showEffects(event.getArena(), event.getTeam());
+	}
+	
+	private void showEffects(Arena arena, ChickenLeagueTeam team) {
+		Bukkit.getScheduler().runTaskLater(Constants.plugin, () -> {
+			new ParticleBuilder(Particle.EXPLOSION_HUGE)
+					.allPlayers()
+					.count(20)
+					.location(arena.getBall().getChicken().getLocation())
+					.offset(4, 4, 4)
+					.spawn();
+			arena.getPlayers().forEach(p -> p.playSound(EXPLOSION));
+		}, 10);
+		fireworkTaskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(Constants.plugin, new Runnable(){
+			int i = 0;
+			@Override
+			public void run() {
+				i++;
+				spwanFireWork(arena, team);
+				if (i == 3) {
+					Bukkit.getScheduler().cancelTask(fireworkTaskID);
+				}
+			}
+
+		}, 10, 30);
+	}
+	
+	private void spwanFireWork(Arena arena, ChickenLeagueTeam team) {
+		Color teamColor = Color.WHITE;
+		if(team == arena.getTeam1()) {
+			teamColor = Color.BLUE;
+		}
+		if(team == arena.getTeam2()) {
+			teamColor = Color.RED;
+		}
+		FireworkEffect effect = FireworkEffect
+				.builder()
+				.flicker(true)
+				.trail(true)
+				.with(Type.BALL_LARGE)
+				.withColor(teamColor, Color.GREEN)
+				.withFade(Color.WHITE)
+				.build();
+		Firework firework = arena.getWorld().spawn(arena.getMiddle(), Firework.class);
+		FireworkMeta meta = firework.getFireworkMeta();
+		meta.addEffect(effect);
+		meta.setPower(1);
+		firework.setFireworkMeta(meta);
 	}
 	
 }
