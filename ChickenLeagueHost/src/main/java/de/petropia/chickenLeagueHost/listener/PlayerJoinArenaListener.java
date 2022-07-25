f0f58431d4d1fc6d7ae59891216c27c89d8d9896
ProfessorSam <Samderprofessor@gmail.com>
2022-07-25 23:38:44 +0200

    show description on start

--- ChickenLeagueHost/src/main/java/de/petropia/chickenLeagueHost/listener/PlayerJoinArenaListener.java
+++ ChickenLeagueHost/src/main/java/de/petropia/chickenLeagueHost/listener/PlayerJoinArenaListener.java
@@ -3,14 +3,18 @@ package de.petropia.chickenLeagueHost.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import de.petropia.chickenLeagueHost.Constants;
 import de.petropia.chickenLeagueHost.arena.GameCountDown;
 import de.petropia.chickenLeagueHost.events.PlayerJoinArenaEvent;
 import de.petropia.chickenLeagueHost.util.MessageSender;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 public class PlayerJoinArenaListener implements Listener {
 	
+	private static final Component DESCRIPTION = Component.text(Constants.config.getString("Description")).color(NamedTextColor.GRAY);
+	private static final Component DESCRIPTION_TITLE = Component.text("ChickenLeague").color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD);
 	private static final Component JOIN_MESSAGE = Component.text(" ist dem Spiel beigetreten!").color(NamedTextColor.GRAY);
 	
 	@EventHandler
@@ -19,6 +23,9 @@ public class PlayerJoinArenaListener implements Listener {
 		if(event.getArena().getPlayers().size() == event.getArena().getMaxPlayers()) {
 			new GameCountDown(10, event.getArena(), false);
 		}
+		event.getPlayer().sendMessage(Component.text(" "));
+		event.getPlayer().sendMessage(DESCRIPTION_TITLE);
+		event.getPlayer().sendMessage(DESCRIPTION);
+		event.getPlayer().sendMessage(Component.text(" "));
 	}
-	
 }
